985e997533397471fe85609303986ed9d2884157
ardallie <ardallie@outlook.com>
2020-07-12 20:38:15 +0100

    added operation

--- src/newbank/test/Calculator.java
+++ src/newbank/test/Calculator.java
@@ -6,6 +6,13 @@ public class Calculator {
     return arg1 + arg2;
   }
 
+  /**
+   * Performs an arithmetical operation: division of two numbers
+   *
+   * @param arg1 number to divide
+   * @param arg2 number to divide by
+   * @return a result of the division
+   */
   public static Integer divider(int arg1, int arg2) {
     if (arg2 != 0) {
       return arg1 / arg2;
