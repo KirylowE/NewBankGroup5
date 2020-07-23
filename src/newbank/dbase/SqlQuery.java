8acc22bc059465ee210b34c809d896911d5bd905
ardallie <ardallie@outlook.com>
2020-07-23 22:27:57 +0100

    comments

--- src/newbank/dbase/SqlQuery.java
+++ src/newbank/dbase/SqlQuery.java
@@ -2,11 +2,19 @@ package newbank.dbase;
 
 /**
  * A type that contains the SQL query string.
- *
- * The IConnect methods such as Create, Read, Update query
- * are accepting this type rather than a String.
  * <p>
- * Currently, it's a minimalistic implementation but it's easily extendable
+ * The IConnect methods such as Create, Read, Update query
+ * accept this type rather than a String.
+ * <p>
+ * * Why do we need this type? It makes the API implementation more clear,
+ * we can make two methods that use the same name e.g. updateEntry():
+ * 1st method accepts an SQL query string, you'll can build and pass any query
+ * 2nd method accepts the field name, content and primary key of item to update,
+ * which can assists with some common, repeated tasks of updating a single field
+ * This is a language feature called method overloading.
+ * <p>
+ * We could use a String instead of SQL type, but when we use the latter, it's clear
+ * that it's not just any string but a query. Currently, it's a minimalistic implementation but it's easily extendable.
  */
 public class SqlQuery {
 
