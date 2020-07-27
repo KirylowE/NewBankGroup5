cd8fc7871b887aef85f28ce20c90d49461eeed55
ardallie <ardallie@outlook.com>
2020-07-27 02:42:11 +0100

    Customer restore point

--- src/newbank/server/Customer.java
+++ src/newbank/server/Customer.java
@@ -9,6 +9,7 @@ public class Customer {
   private String primaryKey;
   private String firstName;
   private String lastName;
+  private String userName;
 
   public Customer(String primaryKey) {
     this.accounts = new ArrayList<>();
@@ -36,10 +37,24 @@ public class Customer {
     this.lastName = lastName;
   }
 
+  public String getFullName() {
+    if (this.firstName != null && this.lastName != null) {
+      return this.firstName + " " + this.lastName;
+    }
+    return null;
+  }
+
+  public String getUserName() {
+    return userName;
+  }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
 
   public String accountsToString() {
     String s = "";
-    for(Account a : accounts) {
+    for (Account a : accounts) {
       s += a.toString() + "\n";
     }
     return s;
@@ -57,11 +72,11 @@ public class Customer {
   // ADD MONEY
   //----
 
-  public String addingMoneyToBalance(String typeAccount,double amountToAdd) {
+  public String addingMoneyToBalance(String typeAccount, double amountToAdd) {
     for (Account a : accounts) {
-      if(a.getAccountName().equals(typeAccount)){
+      if (a.getAccountName().equals(typeAccount)) {
         a.addMoneyToBalance(amountToAdd);
-        return "REQUEST ACCEPTED"+ " - " + " DEPOSIT " + a.getAccountName();
+        return "REQUEST ACCEPTED" + " - " + " DEPOSIT " + a.getAccountName();
       }
     }
     return "REQUEST DENIED" + " - " + " DEPOSIT " + typeAccount;
@@ -71,14 +86,13 @@ public class Customer {
   // WITHDRAW MONEY
   //----
 
-  public String withdrawingMoneyToBalance(String typeAccount,double amountToSubtract) {
+  public String withdrawingMoneyToBalance(String typeAccount, double amountToSubtract) {
     for (Account a : accounts) {
-      if(a.getAccountName().equals(typeAccount)){
-        if(amountToSubtract<a.getBalance() ) {
+      if (a.getAccountName().equals(typeAccount)) {
+        if (amountToSubtract < a.getBalance()) {
           a.subtractMoneyToBalance(amountToSubtract);
-          return "REQUEST ACCEPTED " + " - "  + " WITHDRAW "  + a.getAccountName();
-        }
-        else return "REQUEST DENIED " + " - " + " Not enough money for bank account "+ a.getAccountName()+ " withdrawal";
+          return "REQUEST ACCEPTED " + " - " + " WITHDRAW " + a.getAccountName();
+        } else return "REQUEST DENIED " + " - " + " Not enough money for bank account " + a.getAccountName() + " withdrawal";
       }
     }
     return "REQUEST DENIED" + " - " + " WITHDRAW " + typeAccount;
@@ -88,9 +102,9 @@ public class Customer {
   //  NEW CUSTOMER ACCOUNT
   //----
 
-  public Boolean addNewCustomerAccount(String name){
+  public Boolean addNewCustomerAccount(String name) {
     Boolean accFound = false;
-    for(Account acc: accounts){
+    for (Account acc : accounts) {
       if (acc.getAccountName().equals(name)) {
         accFound = true;
       }
@@ -102,7 +116,7 @@ public class Customer {
   // PAY
   //----
 
-  public Boolean pay(String nameAccountSendsMoney, String ownerAccountNameReceivesMoney,String accountNameReceivesMoney, double amountToTransfer) {
+  public Boolean pay(String nameAccountSendsMoney, String ownerAccountNameReceivesMoney, String accountNameReceivesMoney, double amountToTransfer) {
     for (Account a : accounts) {
       if (a.getAccountName().equalsIgnoreCase(nameAccountSendsMoney)) {
         if (amountToTransfer <= a.getBalance()) {
@@ -119,14 +133,14 @@ public class Customer {
   // MOVE
   //----
 
-  public String move(String accountFrom, String accountTo , double amountToMove) {
+  public String move(String accountFrom, String accountTo, double amountToMove) {
 
-    for ( Account a: accounts) {
-      if(a.getAccountName().equals(accountFrom)) {
-        if(amountToMove<=a.getBalance()){
+    for (Account a : accounts) {
+      if (a.getAccountName().equals(accountFrom)) {
+        if (amountToMove <= a.getBalance()) {
           a.subtractMoneyToBalance(amountToMove);
-          for (Account b:accounts){
-            if(b.getAccountName().equals(accountTo)){
+          for (Account b : accounts) {
+            if (b.getAccountName().equals(accountTo)) {
               b.addMoneyToBalance(amountToMove);
               return "SUCCESS";
             }
